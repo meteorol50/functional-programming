ad0d68f123291627f64049e8c5a068e9ab679a85
meteorol50 <meteorol50.sky@gmail.com>
2022-01-08 17:06:25 +0900

    Add Getting Started

---
+++ src/main/scala/2.GettingStarted.scala
@@ -0,0 +1,62 @@
+// A comment!
+/* Another comment */
+/** A documentation comment */
+object MyModule {
+
+  def abs(n: Int): Int =
+    if (n < 0) -n
+    else n
+
+  private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
+  }
+
+  def main(args: Array[String]): Unit =
+    println(formatAbs(-42))
+
+  // A definition of factorial, using a local, tail recursive function
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n-1, n*acc)
+
+    go(n, 1)
+  }
+
+  // Exercise 1: Write a function to compute the nth fibonacci number
+  def fib(n: Int): Int = {
+    def go(n: Int, acc1: Int, acc2: Int): Int =
+      if (n == 0) acc1
+      else go(n - 1, acc2, acc1 + acc2)
+
+    go(n, 0, 1)
+  }
+
+  // This definition and `formatAbs` are very similar..
+  private def formatFactorial(n: Int) = {
+    val msg = "The factorial of %d is %d."
+    msg.format(n, factorial(n))
+  }
+
+  // We can generalize `formatAbs` and `formatFactorial` to
+  // accept a _function_ as a parameter
+  def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d."
+    msg.format(name, n, f(n))
+  }
+
+}
+
+object FormatAbsAndFactorial {
+
+  import MyModule._
+
+  // Now we can use our general `formatResult` function
+  // with both `abs` and `factorial`
+  def main(args: Array[String]): Unit = {
+    println(formatResult("absolute value", -42, abs))
+    println(formatResult("factorial", 7, factorial))
+  }
+}
