71b7e3697e61ffc4aef51ab85f50544e9cb343fc
meteorol50 <meteorol50.sky@gmail.com>
2022-01-10 12:45:57 +0900

    Add Polymorphic Function

--- src/main/scala/2.GettingStarted.scala
+++ src/main/scala/2.GettingStarted.scala
@@ -27,6 +27,7 @@ object MyModule {
 
   // Exercise 1: Write a function to compute the nth fibonacci number
   def fib(n: Int): Int = {
+    @annotation.tailrec
     def go(n: Int, acc1: Int, acc2: Int): Int =
       if (n == 0) acc1
       else go(n - 1, acc2, acc1 + acc2)
@@ -59,4 +60,93 @@ object FormatAbsAndFactorial {
     println(formatResult("absolute value", -42, abs))
     println(formatResult("factorial", 7, factorial))
   }
+}
+
+object TestFib {
+
+  import MyModule._
+
+  // test implementation of `fib`
+  def main(args: Array[String]): Unit = {
+    println("Expected: 0, 1, 1, 2, 3, 5, 8")
+    println("Actual:   %d, %d, %d, %d, %d, %d, %d".format(fib(0), fib(1), fib(2), fib(3), fib(4), fib(5), fib(6)))
+  }
+}
+
+// Functions get passed around so often in FP that it's
+// convenient to have syntax for constructing a function
+// *without* having to give it a name
+object AnonymousFunctions {
+  import MyModule._
+
+  // Some examples of anonymous functions:
+  def main(args: Array[String]): Unit = {
+    println(formatResult("absolute value", -42, abs))
+    println(formatResult("factorial", 7, factorial))
+    println(formatResult("increment", 7, (x: Int) => x + 1))
+    println(formatResult("increment2", 7, (x) => x + 1))
+    println(formatResult("increment3", 7, x => x + 1))
+    println(formatResult("increment4", 7, _ + 1))
+    println(formatResult("increment5", 7, x => { val r = x + 1; r }))
+  }
+}
+
+object MonomorphicBinarySearch {
+
+  // First, a binary search implementation, specialized to `Double`,
+  // another primitive type in Scala, representing 64-bit floating
+  // point numbers
+  // Ideally, we could generalize this to work for any `Array` type,
+  // so long as we have some way of comparing elements of the `Array`
+  def binarySearch(ds: Array[Double], key: Double): Int = {
+    @annotation.tailrec
+    def go(low: Int, mid: Int, high: Int): Int = {
+      if (low > high) -mid - 1
+      else {
+        val mid2 = (low + high) / 2
+        val d = ds(mid2) // We index into an array using the same
+        // syntax as function application
+        if (d == key) mid2
+        else if (d > key) go(low, mid2, mid2-1)
+        else go(mid2 + 1, mid2, high)
+      }
+    }
+    go(0, 0, ds.length - 1)
+  }
+
+}
+
+object PolymorphicFunctions {
+
+  // Here's a polymorphic version of `binarySearch`, parameterized on
+  // a function for testing whether an `A` is greater than another `A`.
+  def binarySearch[A](as: Array[A], key: A, gt: (A, A) => Boolean): Int = {
+    @annotation.tailrec
+    def go(low: Int, mid: Int, high: Int): Int = {
+      if (low > high) -mid - 1
+      else {
+        val mid2 = (low + high) / 2
+        val a = as(mid2)
+        val greater = gt(a, key)
+        if (!greater && !gt(key, a)) mid2
+        else if (greater) go(low, mid2, mid2 - 1)
+        else go(mid2 + 1, mid2, high)
+      }
+    }
+
+    go(0, 0, as.length - 1)
+  }
+
+  // Exercise 2: Implement a polymorphic function to check whether
+  // an `Array[A]` is sorted
+  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(n: Int): Boolean = {
+      if (n + 1 == as.length) true
+      else if (gt(as(n), as(n + 1))) false
+      else loop(n + 1)
+    }
+    loop(0)
+  }
+
 }
