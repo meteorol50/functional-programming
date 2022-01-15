ed1d7b96675d1afb6934cb03747032aa9998140d
meteorol50 <meteorol50.sky@gmail.com>
2022-01-15 17:07:03 +0900

    Add List By Ex3.5

--- src/main/scala/DataStructures/List.scala
+++ src/main/scala/DataStructures/List.scala
@@ -1,5 +1,59 @@
 package DataStructures
 
+sealed trait List[+A]                 // `List` data type, parameterized on a type, `A`
+case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
+/* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
+which may be `Nil` or another `Cons`.
+ */
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+// `List` companion object. Contains functions for creating and working with lists.
 object List {
+
+  def sum(ints: List[Int]): Int = ints match {  // A function that uses pattern matching to add up a list of integers
+    case Nil => 0                               // The sum of the empty list is 0.
+    case Cons(x,xs) => x + sum(xs)              // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  // Variadic function syntax
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+  val x = List(1, 2, 3, 4, 5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(h, t) => h + sum(t)
+    case _ => 101
+  }
+
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => sys.error("tail of empty list")
+    case Cons(_, t) => t
+  }
+
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => sys.error("setHead on empty list")
+    case Cons(_, t) => Cons(h, t)
+  }
+
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_, t) => drop(t, n - 1)
+    }
+
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Cons(h, t) if f(h) => dropWhile(t, f)
+    case _ => l
+  }
 
 }
