900dd15df17fd3f8cb34148c10725e47f8092625
meteorol50 <meteorol50.sky@gmail.com>
2022-01-23 11:48:40 +0900

    Add Ex4.6

--- src/main/scala/ErrorHandling/Either.scala
+++ src/main/scala/ErrorHandling/Either.scala
@@ -1,5 +1,48 @@
 package ErrorHandling
 
+sealed trait Either[+E,+A] {
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Right(a) => Right(f(a))
+    case Left(e) => Left(e)
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Right(a) => f(a)
+    case Left(e) => Left(e)
+  }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case Right(a) => Right(a)
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {
+      a <- this
+      b2 <- b
+    } yield f(a, b2)
+}
+case class Left[+E](get: E) extends Either[E,Nothing]
+case class Right[+A](get: A) extends Either[Nothing,A]
+
 object Either {
+
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
+
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
+
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] =
+    if (xs.isEmpty)
+      Left("mean of empty list!")
+    else
+      Right(xs.sum / xs.length)
+
+  def safeDiv(x: Int, y: Int): Either[Exception, Int] =
+    try Right(x / y)
+    catch { case e: Exception => Left(e) }
+
+  def Try[A](a: => A): Either[Exception, A] =
+    try Right(a)
+    catch { case e: Exception => Left(e) }
 
 }
