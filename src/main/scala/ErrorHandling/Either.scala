e05241bfaf3656cfda58cd1db759b6ca58d28597
meteorol50 <meteorol50.sky@gmail.com>
2022-01-23 12:02:57 +0900

    Add Ex4.7 ~ 4.8

--- src/main/scala/ErrorHandling/Either.scala
+++ src/main/scala/ErrorHandling/Either.scala
@@ -1,5 +1,9 @@
 package ErrorHandling
 
+import ErrorHandling.Option.{map2, traverse}
+
+import scala.::
+
 sealed trait Either[+E,+A] {
   def map[B](f: A => B): Either[E, B] = this match {
     case Right(a) => Right(f(a))
@@ -27,22 +31,46 @@ case class Right[+A](get: A) extends Either[Nothing,A]
 
 object Either {
 
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
-
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
-
   def mean(xs: IndexedSeq[Double]): Either[String, Double] =
-    if (xs.isEmpty)
-      Left("mean of empty list!")
-    else
-      Right(xs.sum / xs.length)
+  if (xs.isEmpty)
+  Left("mean of empty list!")
+  else
+  Right(xs.sum / xs.length)
 
   def safeDiv(x: Int, y: Int): Either[Exception, Int] =
-    try Right(x / y)
-    catch { case e: Exception => Left(e) }
+  try Right(x / y)
+  catch { case e: Exception => Left(e) }
 
   def Try[A](a: => A): Either[Exception, A] =
-    try Right(a)
-    catch { case e: Exception => Left(e) }
+  try Right(a)
+  catch { case e: Exception => Left(e) }
+
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = es match {
+    case Nil => Right(Nil)
+    case h :: t => (f(h) map2(traverse(t)(f)))(_ :: _)
+  }
+
+  def traverse_1[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    es.foldRight[Either[E,List[B]]](Right(Nil))((a, b) => f(a).map2(b)(_ :: _))
+
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] =
+    traverse(es)(x => x)
+
+  /*
+  There are a number of variations on `Option` and `Either`. If we want to accumulate multiple errors, a simple
+  approach is a new data type that lets us keep a list of errors in the data constructor that represents failures:
+
+  trait Partial[+A,+B]
+  case class Errors[+A](get: Seq[A]) extends Partial[A,Nothing]
+  case class Success[+B](get: B) extends Partial[Nothing,B]
+
+  There is a type very similar to this called `Validation` in the Scalaz library. You can implement `map`, `map2`,
+  `sequence`, and so on for this type in such a way that errors are accumulated when possible (`flatMap` is unable to
+  accumulate errors--can you see why?). This idea can even be generalized further--we don't need to accumulate failing
+  values into a list; we can accumulate values using any user-supplied binary function.
+
+  It's also possible to use `Either[List[E],_]` directly to accumulate errors, using different implementations of
+  helper functions like `map2` and `sequence`.
+  */
 
 }
