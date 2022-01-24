d3e32be5c3bfc4603c30038d20b49077b9724852
meteorol50 <meteorol50.sky@gmail.com>
2022-01-24 23:50:26 +0900

    Add Ex5.2

--- src/main/scala/Laziness/Stream.scala
+++ src/main/scala/Laziness/Stream.scala
@@ -11,6 +11,25 @@ trait Stream[+A] {
     go(this, List()).reverse
   }
 
+  /*
+  In order to avoid the `reverse` at the end, we could write it using a
+  mutable list buffer and an explicit loop instead. Note that the mutable
+  list buffer never escapes our `toList` method, so this function is
+  still _pure_.
+  */
+  def toListFast: List[A] = {
+    val buf = new collection.mutable.ListBuffer[A]
+    @annotation.tailrec
+    def go(s: Stream[A]): List[A] = s match {
+      case Cons(h,t) =>
+        buf += h()
+        go(t())
+      case _ => buf.toList
+    }
+    go(this)
+  }
+
+
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
       case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
@@ -25,9 +44,17 @@ trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
-  def take(n: Int): Stream[A] = ???
 
-  def drop(n: Int): Stream[A] = ???
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
+    case Cons(h, _) if n == 1 => cons(h(), empty)
+    case _ => empty
+  }
+
+  final def drop(n: Int): Stream[A] = this match {
+    case Cons(_, t) if n > 0 => t().drop(n - 1)
+    case _ => this
+  }
 
   def takeWhile(p: A => Boolean): Stream[A] = ???
 
